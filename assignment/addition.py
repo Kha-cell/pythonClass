c877f5c7aeb3d8abb47421cd0d15e79df1b5dc86
kha-cell <adekanbikhalid62@gmail.com>
2024-10-18 20:28:23 +0100

    update assignment

---
+++ assignment/addition.py
@@ -0,0 +1,8 @@
+num1 = 27.5
+num2 = 2
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+print("Floor Division:", num1 // num2)
+print("Exponentiation:", num1 ** num2)
