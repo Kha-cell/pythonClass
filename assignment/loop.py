c53c79d7a593ff7650e2a456b63c5dc0f5f7fd2f
kha-cell <adekanbikhalid62@gmail.com>
2024-10-20 23:19:59 +0100

    update assignment

---
+++ assignment/loop.py
@@ -0,0 +1,10 @@
+i = 1
+
+while i < 6:
+
+Print (i)
+
+if i == 3:
+
+break
+i += 1
---
+++ assignment/twonumbers.py
@@ -0,0 +1,3 @@
+number1= int(input("enter fisrt number"))
+number2= int(input("enter second number"))
+print (number1 * number2)
